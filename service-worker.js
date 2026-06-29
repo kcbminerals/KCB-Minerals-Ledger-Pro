2cf0cf7bcde65cfedea5dbc905a6ac63df13e9ca
kcbminerals <kcbminerals@gmail.com>
2026-06-29 23:00:29 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-// v6.5 no-cache service worker for KCB Minerals Ledger Pro
-self.addEventListener("install", event => self.skipWaiting());
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request, { cache: "no-store" }).catch(() => fetch(event.request)));
-});
